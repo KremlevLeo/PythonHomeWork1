8f60a904b3c9dfee7fc877002492105260da00a6
Leonid Kremlev <leeonidix@gmail.com>
2022-10-02 19:07:30 +0500

    Done Task 004

--- Task003.py
+++ Task003.py
@@ -1,15 +1,24 @@
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+
 def Part(x, y):
     tmp = 0
     if x > 0 and y > 0:
         tmp = 1
+        print(f"The point is in the {tmp} quarter")
     if x < 0 and y > 0:
         tmp = 2
+        print(f"The point is in the {tmp} quarter")
     if x < 0 and y < 0:
         tmp = 3
+        print(f"The point is in the {tmp} quarter")
     if x > 0 and y < 0:
         tmp = 4
-    print(f"The point is in the {tmp} quarter")
+        print(f"The point is in the {tmp} quarter")
+    if x == 0 and y > 0 or y < 0:
+        print("Point on X axis")
+    if y == 0 and x > 0 or x < 0:
+        print("Point on Y axis")
 
 x = int(input("Enter X: "))
 y = int(input("Enter Y: "))
-Part(x,y)
+Part(x, y)
---
+++ Task004.py
@@ -0,0 +1,14 @@
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+def Result(quater):
+    if quater == 1:
+        print("X>0 ; Y>0")
+    if quater == 2:
+        print("X<0 ; Y>0")
+    if quater == 3:
+        print("X<0 ; Y<0")
+    if quater == 4:
+        print("X>0 ; Y<0")
+
+quater = int(input("Enter number of quater: "))
+Result(quater)
