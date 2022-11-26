4d36e75ca3ba8a5cf5021e0648907bcc500a4018
Леонид Кремлев <62460369+KremlevLeo@users.noreply.github.com>
2022-11-26 11:49:11 +0500

    Update Task005.py

--- Task005.py
+++ Task005.py
@@ -1,7 +1,7 @@
 # Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 
 
-def Dis_betw_point(Ax, Ay, Bx, By):
+def dis_betw_point(Ax, Ay, Bx, By):
     res = ((Bx-Ax)**2+(By-Ay)**2)**0.5
     res = round(res, 2)
     print(f"A ({Ax},{Ay}); B({Bx},{By}) -> {res}")
@@ -12,4 +12,4 @@ Ay = int(input("Enter Ay: "))
 Bx = int(input("Enter Bx: "))
 By = int(input("Enter By: "))
 
-Dis_betw_point(Ax, Ay, Bx, By)
+dis_betw_point(Ax, Ay, Bx, By)
